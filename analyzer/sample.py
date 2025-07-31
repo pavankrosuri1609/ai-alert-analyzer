a165b27d45ec1408ba64a2a49552c5855c192bf4
gatla.manohar <gatla.manohar@pragmaedge.com>
2025-07-31 23:32:06 +0800

    delete

--- analyzer/sample.py
+++
@@ -1,5 +0,0 @@
-from langchain.chat_models import ChatOpenAI
-
-llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
-print(llm.predict("Summarize this: 5 errors occurred while connecting to MQ server."))
-
